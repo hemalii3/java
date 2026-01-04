0746f6df94482078e865372686e91ad6f1293a52
hemali <hemal@pop-os.fritz.box>
2026-01-04 16:45:02 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#java progress
+1. first java program java1.java
---
+++ java1.java
@@ -0,0 +1,6 @@
+public class java1 {
+    public static void main(String[] args) {
+        System.out.println("hellow java!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
